ab54c3544cff8bfe29040c7112a25c0fd3c43b7a
Suyash Dhakal <suyash.dhakal@icloud.com>
2024-12-13 17:39:22 +0545

    Set up project structure

---
+++ index.js
@@ -0,0 +1,9 @@
+const { log } = require('console');
+const express=require('express');
+const PORT=8000;
+
+const app=express();
+
+app.listen(PORT,()=>{
+    console.log(`Server started at PORT:${PORT}`);
+});
